6f40b754e86ffb8d0440117101c74b6e648cceda
Xilobone <xilobone@gmail.com>
2025-01-16 20:34:39 +0100

    Option to display certain bitboards

--- chess/gui/ChessGUI.cs
+++ chess/gui/ChessGUI.cs
@@ -8,6 +8,9 @@ namespace gui
         private static readonly int[] BOARD_OFFSET = [64, 64];
         private static readonly int SQUARE_SIZE = 64;
 
+        private ComboBox bitboardComboBox;
+        long bitboard = 0;
+
         private static Dictionary<int, int> IMAGE_INDEX = new Dictionary<int, int>() {
             {Piece.WHITE_KING, 0},
             {Piece.WHITE_QUEEN, 1},
@@ -29,7 +32,7 @@ namespace gui
 
         private Color light = Color.FromArgb(219, 198, 140);
         private Color dark = Color.FromArgb(128, 100, 25);
-        private Color bitmapColor = Color.FromArgb(255, 10, 10);
+        private Color bitboardColor = Color.FromArgb(255, 10, 10);
 
         private Board? currentBoard;
 
@@ -53,6 +56,39 @@ namespace gui
             Text = "Chess GUI";
             Size = new Size(600, 600);
 
+            bitboardComboBox = new ComboBox();
+
+            //center combobox above the board
+            bitboardComboBox.Location = new Point(BOARD_OFFSET[0] + 4 * SQUARE_SIZE - bitboardComboBox.Width / 2, BOARD_OFFSET[1] / 2 - bitboardComboBox.Height / 2);
+            bitboardComboBox.DropDownStyle = ComboBoxStyle.DropDownList;
+
+            bitboardComboBox.Items.AddRange([
+                "white: pawn",
+                "white: rook",
+                "white: knight"]);
+
+            bitboardComboBox.SelectedValueChanged += OnSelectBitboard;
+            Controls.Add(bitboardComboBox);
+
+        }
+
+        private void OnSelectBitboard(object? sender, EventArgs e)
+        {
+            if (bitboardComboBox.SelectedItem == null || currentBoard == null)
+            {
+                bitboard = 0;
+                return;
+            }
+
+            string? selectedBitboard = bitboardComboBox.SelectedItem.ToString();
+
+            switch (selectedBitboard)
+            {
+                case "white: pawn": bitboard = currentBoard.bitboardsWhite[Board.BITBOARD_PAWN]; break;
+                case "white: rook": bitboard = currentBoard.bitboardsWhite[Board.BITBOARD_ROOK]; break;
+                case "white: knight": bitboard = currentBoard.bitboardsWhite[Board.BITBOARD_KNIGHT]; break;
+                default: bitboard = 0; break;
+            }
         }
 
         protected override void OnPaint(PaintEventArgs e)
@@ -78,21 +114,19 @@ namespace gui
             lightBrush.Dispose();
             darkBrush.Dispose();
 
-            DrawAttackMap(g);
+            DrawBitboard(g);
             DrawPieces(g);
         }
 
-        private void DrawAttackMap(Graphics g)
+        private void DrawBitboard(Graphics g)
         {
-            if (currentBoard == null) return;
-
-            Brush brush = new SolidBrush(bitmapColor);
-            long attackMap = currentBoard.attackMapWhite;
+            Brush brush = new SolidBrush(bitboardColor);
+            long btb = bitboard;
 
             for (int i = 0; i < 64; i++)
             {
-                bool isAttacking = (attackMap & 1) == 1;
-                attackMap >>= 1;
+                bool isAttacking = (btb & 1) == 1;
+                btb >>= 1;
 
                 if (!isAttacking)
                 {
